3ae817639c374da75a37519cce8651b445e86ced
Dan Kostiuk <dank216@gmail.com>
2016-09-20 22:46:13 -0400

    Fixes #1 - Users can now register (added register form validation) - Added ng2-toastr notifications

--- WebContent/app/register.component.ts
+++ WebContent/app/register.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import { RegistrationService } from './registration.service';
 import { RegistrationInfo } from './registration-info'
 
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+
 @Component({
     selector: 'my-register',
     templateUrl: 'app/register.component.html',
@@ -12,9 +14,12 @@ import { RegistrationInfo } from './registration-info'
 
 export class RegisterComponent {
     regInfo: RegistrationInfo;
+    error: any;
+    confPassword: string;
 
     constructor(private router: Router,
-                private regService: RegistrationService) {
+                private regService: RegistrationService,
+                private toastr: ToastsManager) {
 
         var regInfo: RegistrationInfo = {
             username: "",
@@ -33,7 +38,54 @@ export class RegisterComponent {
     }
 
     register(regInfo: RegistrationInfo) {
+
+        if (!regInfo.username || regInfo.username.length == 0) {
+            this.error = "Email must not be blank."
+            return;
+        }
+
+        if (!regInfo.password || regInfo.password.length == 0) {
+            this.error = "Password must not be blank."
+            return;
+        }
+
+        if (regInfo.password != this.confPassword) {
+            this.error = "Passwords must match."
+            return;
+        }
+
+        if (!regInfo.firstName || regInfo.firstName.length == 0) {
+            this.error = "First name must not be blank."
+            return;
+        }
+
+        if (!regInfo.lastName || regInfo.lastName.length == 0) {
+            this.error = "Last name must not be blank."
+            return;
+        }
+
+        if (!regInfo.phone || regInfo.phone.length == 0) {
+            this.error = "Phone number must not be blank."
+            return;
+        } else {
+            if (isNaN(regInfo.phone) || regInfo.phone.length != 10) {
+                this.error = "Phone number must be a 10-digit number."
+                return;
+            }
+        }
+
+        if (!regInfo.address || regInfo.address.length == 0) {
+            this.error = "Company address must not be blank."
+            return;
+        }
+
         this.regService.register(regInfo);
+        this.showSuccess(regInfo.username);
+    }
+
+    showSuccess(username) {
+        this.toastr.success('Registered user ' + username + ' successfully')
+        this.gotoLogin();
     }
 
 }
